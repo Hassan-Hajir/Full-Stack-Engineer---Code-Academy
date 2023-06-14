bc4adfe2b778cb0d485d4329fe15fb6898884eda
HajirDev <69417566+HajirDev@users.noreply.github.com>
2023-06-14 13:56:16 +0300

    updated styling

--- JavaScript/JavaScript Projects/Counting App/index.js
+++ JavaScript/JavaScript Projects/Counting App/index.js
@@ -1,17 +1,15 @@
-let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
-
+let countEl = document.getElementById("count-el");
 let count = 0;
-let resetCount = count - count;
 
 function increment() {
-  count = count + 1;
-  countEl.innerText = count;
+  count += 1;
+  countEl.textContent = count;
 }
 
 function save() {
-  // let countStr = count + " - ";
-  saveEl.textContent += count + " - ";
-  countEl.textContent = 0; //reseting
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
   count = 0;
 }
